900ae27ac869303a0ec04980c4eac7bb45bfd36e
Georgy Bunin <bunin.co.il@gmail.com>
2021-11-08 16:04:48 +0200

    fix reset data

--- server.js
+++ server.js
@@ -16,17 +16,27 @@ const initData = () => [...Array(100).keys()].map(id => ({
 
 let data = initData();
 
-const getUsers = () => data.map(({id, firstName, lastName, date, phone}) => ({id, firstName, lastName, date, phone}));
+const getUsers = () => {
+  return data.map(({id, firstName, lastName, date, phone}) => ({id, firstName, lastName, date, phone}));
+}
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/reset', (req, res) => data = initData() && res.json({success: true}));
+app.get('/reset', (req, res) => {
+  data = initData();
+  console.log(JSON.stringify(data));
+  res.json({success: true});
+});
+
 app.get('/users', (req, res) => res.json(getUsers()));
+
 app.get('/users/:id', (req, res) => res.json(data.find(item => item.id.toString() === req.params.id)));
+
 app.post('/login', (req, res) => res.json({token: Math.random().toString(36).substr(2, 9)}));
+
 app.post('/users/:id', (req, res) => {
   const index = data.findIndex(item => item.id.toString() === req.params.id);
   if (index === -1) {
